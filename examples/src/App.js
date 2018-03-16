ca48f4c88f45c7509553175951beaf725abca73e
liuwenzhu <18339967951@163.com>
2018-03-16 12:44:51 +0800

    update
    Signed-off-by: liuwenzhu <18339967951@163.com>

---
+++ examples/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>aaaaaaaa</h1>
+        <h1>aaaaaaaa</h1>
+        <h1>aaaaaaaa</h1>
+        <h1>aaaaaaaa</h1>
+        <h1>aaaaaaaa</h1>
+        <h1>aaaaaaaa</h1>
+        <h1>aaaaaaaa</h1>
+        <h1>aaaaaaaa</h1>
+        <h1>aaaaaaaa</h1>
+        <h1>aaaaaaaa</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
